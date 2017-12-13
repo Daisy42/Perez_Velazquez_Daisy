d8f414e16aac5fc7606cce6da9f34d217fc3cbc2
Daisy42 <day_edu@hotmail.com>
2017-12-12 21:17:53 -0600

    cambio factorial

--- HOLAMUNDO.cpp
+++ HOLAMUNDO.cpp
@@ -2,5 +2,16 @@
 using namespace std;
 int main(){
 	cout<<"HOLA MUNDO"<<endl;
+	
+	int num=0, fac=0;
+	cout<<"Ingresa un numero"<<endl;
+	cin>>num;
+	fac=num;
+	for(int i=num-1;i>=1;i--){
+		fac=fac*i;
+	}
+	cout<<"El factorial de"<<" "<<num<<" "<<"es"<<" "<<fac<<endl;
+	system("PAUSE");
 	return 0;
+
 }
